3ecc5caced3629256afb96e3aeaf699d5977194b
sato604 <satoberu@gmail.com>
2021-11-09 02:47:10 +0900

    prepare: Nov_semi

--- app/src/main/java/lab/haze/SecurityMaster/Controller/LearnController.java
+++ app/src/main/java/lab/haze/SecurityMaster/Controller/LearnController.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lab.haze.SecurityMaster.Model.User;
 @Controller
@@ -33,7 +35,38 @@ public class LearnController {
     @GetMapping("/learn/1injection/lecture")
     public String lecture1(){
         return "/learn/1injection/lecture";
+    
     }
+
+    @GetMapping("/learn/1injection/test1")
+    public String injtest1(){
+        return "/learn/1injection/test1";
+    }
+
+    @PostMapping("/learn/1injection/test1")
+    public String postTest1(@RequestParam("ans") String ans) {
+        int ians = Integer.parseInt(ans);
+        if(ians != 4){
+            return "/learn/1injection/inc1";
+        }else
+        {
+            return "/learn/1injection/col1";
+        }
+    }
+    @GetMapping("/learn/1injection/inc1")
+    public String injInc1(){
+        //企業価値変更のコード
+        return "/learn/1injection/inc1";
+    }
+
+    @GetMapping("/learn/1injection/col1")
+    public String injCol1(){
+        //企業価値変更のコード
+        return "/learn/1injection/col1";
+    }
+
+
+    
 
     
 }
